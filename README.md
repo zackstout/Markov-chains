d25e5495ee24717661215b1ff4c5f551aeb95e37
Zack Stout <zackstout@gmail.com>
2018-03-14 23:28:28 -0500

    great, we seem to be implementing a markov chain

---
+++ README.md
@@ -0,0 +1,2 @@
+# Markov Chains
+Translating the logic behind matrix multiplication into code so that I can model some Markov chains given probability datasets.
--- client.js
+++ client.js
@@ -1,9 +1,14 @@
 
-// A 5x2 matrix (first matrix, array of rows);
+// A 5x2 matrix (first matrix, array of rows, 5 elements of 2);
 var m1 = [[1, 2], [2, 3], [3, 1], [4, 0.42], [2, 3.14]];
 
-// A 2x4 matrix (second matrix, array of cols):
+// A 2x4 matrix (second matrix, array of cols, 4 elements of 2):
 var m2 = [[5, 2.5], [2, 3], [3,2], [3.2,1]];
+
+
+var m3 = [[0.8, 0.2], [0.15, 0.85]];
+
+// It's annoying that they're not formatted the same, but we get the benefit that the length of their element-arrays is always the same.
 
 // A matrix is either an array of rows (first argument a) or an array of cols (second argument b):
 function matrixMult(a, b) {
@@ -28,7 +33,8 @@ function matrixMult(a, b) {
     mOut.push(row);
   }
 
-  console.log(mOut);
+  // console.log(mOut);
+  return mOut;
 }
 
 
@@ -41,4 +47,17 @@ function multRows(row, col) {
   return out;
 }
 
-matrixMult(m1, m2);
+// matrixMult(m1, m2);
+
+var m = m3;
+
+var i = 10;
+
+while (i > 0) {
+  // Let's check to ensure that order doesn't matter here:
+  m = matrixMult(m3, m);
+  console.log(m);
+  i--;
+}
+// var m = matrixMult(m3, m3);
+// matrixMult(m, m3);
